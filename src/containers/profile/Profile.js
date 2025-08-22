d1406eb94e7f3284b4f423226522d8a57b4cf6d3
devsugandh <sugandh@hipster-inc.com>
2025-08-22 12:24:55 +0530

    Force Profile component to always show Reach Out to me! section

--- src/containers/profile/Profile.js
+++ src/containers/profile/Profile.js
@@ -36,17 +36,10 @@ export default function Profile() {
       getProfileData();
     }
   }, []);
-  if (
-    openSource.display &&
-    openSource.showGithubProfile === "true" &&
-    !(typeof prof === "string" || prof instanceof String)
-  ) {
-    return (
-      <Suspense fallback={renderLoader()}>
-        <GithubProfileCard prof={prof} key={prof.id} />
-      </Suspense>
-    );
-  } else {
-    return <Contact />;
-  }
+  // Always show the GithubProfileCard (Reach Out to me! section)
+  return (
+    <Suspense fallback={renderLoader()}>
+      <GithubProfileCard prof={prof} key={prof.id} />
+    </Suspense>
+  );
 }
